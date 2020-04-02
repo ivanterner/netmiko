a32511d91d082731c9f96f8b583b8db93777d015
ivanterner <ivanterner@icloud.com>
2020-04-02 22:25:16 +0500

    add for and list

--- ios.py
+++ ios.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env  python3
 
-from netmiko import ConnectHandler
-import getpass 
-import sys
+import getpass
 
-hostname = input("Hostname:")
+from netmiko import ConnectHandler
+
+hostname_list = ["sw1", "sw2", "sw3"]
 username = input("Username:")
 password = getpass.getpass()
 enable_pass = getpass.getpass(prompt='Enter enable password:')
-command  = input("Please raw command:")
+command = input("Please raw command:")
 
-device = ConnectHandler(device_type='cisco_ios',ip=hostname,username=username,password=password)
-output = device.send_command(command)
-print(output)
+for ip in hostname_list:
+    device = ConnectHandler(device_type='cisco_ios', ip=ip, username=username, password=password)
+    output = device.send_command(command)
+    print(output)
