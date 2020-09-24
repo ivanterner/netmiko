9deab8f84100490e63679dc5fbfd1ff4e8e48f4b
ivanterner <ivanterner@icloud.com>
2020-09-24 09:33:51 +0500

    add file devices.txt

--- conf_ios.py
+++ conf_ios.py
@@ -3,22 +3,21 @@
 import getpass
 
 from netmiko import ConnectHandler
+hostname_list=[]
+file = open("devices.txt","r")
+for item in file:
+    item=item.strip()
+    hostname_list.append(item)
+file.close()
+print(hostname_list)
 
-hostname_list = ["sw1", "sw10", "sw11",
-                 "sw12", "sw14", "sw15",
-                 "sw16", "sw18", "sw19",
-                 "sw2" , "sw20", "sw21",
-                 "sw22", "sw23", "sw24",
-                 "sw25", "sw3" ,"sw31",
-                 "sw32", "sw33" ,"sw5",
-                 "sw6", "sw9", "swvoip1",
-                 "swvoip2", "swvoip3", "swvoip4"]
 username = input("Username:")
 password = getpass.getpass()
 #enable_pass = getpass.getpass(prompt='Enter enable password:')
-command = ["no logging console"]
+command = ["ip name-server 172.16.10.2"]
 
 for ip in hostname_list:
+    print(ip)
     device = ConnectHandler(device_type='cisco_ios', ip=ip, username=username, password=password)
     output = device.send_config_set(command)
     print(output)
